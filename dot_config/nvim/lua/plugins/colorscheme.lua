2bc95a3c452f2d137762d072967422d5fe1ac742
Masaki Monden <monden.masaki@volumetric.jp>
2025-08-09 11:14:33 +0900

    lightテーマを明示的に指定

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
     "sainnhe/edge",
     lazy = false,
     priority = 1000,
+    init = function()
+        vim.o.background = "light"
+    end,
     opts = function()
         vim.g.edge_disable_italic_comment = 1
         vim.cmd.colorscheme("edge")
