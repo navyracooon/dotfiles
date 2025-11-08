7e443f78620939b4417398edc6923c0356e97f91
Masaki Monden <monden.masaki@volumetric.jp>
2025-11-08 16:16:52 +0900

    Flash導入

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,20 +1,22 @@
 return require("lazy").setup({
     -- Colorscheme
     require("plugins.colorscheme"),
-    -- Filer
-    require("plugins.oil"),
-    -- Telescope
-    require("plugins.telescope"),
     -- Comment
     require("plugins.comment"),
-    -- Visible Indent
-    require("plugins.ibl"),
-    -- Status line
-    require("plugins.lualine"),
     -- Completion
     require("plugins.completion"),
     -- Editorconfig
     require("plugins.editorconfig"),
+    -- Filer
+    require("plugins.oil"),
+    -- Flash
+    require("plugins.flash"),
     -- LSP
     require("plugins.lsp"),
+    -- Status line
+    require("plugins.lualine"),
+    -- Telescope
+    require("plugins.telescope"),
+    -- Visible Indent
+    require("plugins.ibl"),
 })
