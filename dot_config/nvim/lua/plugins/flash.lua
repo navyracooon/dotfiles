e96592f2506ab79a09adb49de49d06bb2b9d9078
Masaki Monden <monden.masaki@volumetric.jp>
2025-11-13 16:25:41 +0900

    oil/flashのバインド変更

--- dot_config/nvim/lua/plugins/flash.lua
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -2,24 +2,7 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {},
-    dependencies = {
-        {
-            "nvim-treesitter/nvim-treesitter",
-            branch = "master",
-            lazy = false,
-            build = ":TSUpdate",
-            opts = {
-                highlight = { enable = true },
-                indent = { enable = true },
-                auto_install = true,
-            },
-        },
-    },
     keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        { "<leader>f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     },
 }
--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
     "stevearc/oil.nvim",
     opts = {},
     keys = {
-        { "<leader>f", "<cmd>Oil<CR>" },
+        { "<leader>o", "<cmd>Oil<CR>" },
     },
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     lazy = false,
